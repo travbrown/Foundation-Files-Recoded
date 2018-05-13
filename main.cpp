083702677d67b002c072756cd248641cc4a16121
Travis Brown <travjbrown98@gmail.com>
2018-05-13 16:49:42 -0400

    Main Menu

--- main.cpp
+++ main.cpp
@@ -34,7 +34,43 @@ int main(){
             c. Delete / Rewrite without them
         5. Quit / End While Loop
     */
+   do{
+    int choice=0, Playr_count=0; system("color F0"); 
+    cout << "\t\tWelcome to Foundation Files\n\n\n";
+    cout << "Please enter the number that corresponds with your choice.";
+    cout << "\n\t1. Add Player\n\t2. Edit Player\n\t3. View Player\n\t4. Delete Player\n\t";
+    cout << "5. Quit\n";
+    cout << "You selected: ";
+    cin >> choice;
     
+    switch(choice){
+            case 1:
+                system("clear");
+                //Call add();
+                break;
+            case 2:
+                system("clear");
+                //Call add();
+                break;
+            case 3:
+                system("clear");
+                //Call add();
+                break;
+            case 4:
+                system("clear");
+                //Call add();
+                break;
+            case 5:
+                system("clear");
+                cout << "Thank you for using Foundation Files. Ball is life" << endl;
+                choice = 5;
+                break;
+            default:
+                system("clear");
+                cout << "I'm sorry this option does not exist. Please try again."
+                break;
+    }
+   }while(choice != 5);
 
     return 0;
 }
