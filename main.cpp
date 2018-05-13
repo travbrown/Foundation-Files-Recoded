4649ed78a18cf553d451f68c3ed950ae257dc079
Travis Brown <travjbrown98@gmail.com>
2018-05-13 03:04:35 -0400

    first commit, program structure outlined

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+    /*
+    Main Menu:
+    -Intro Prompt-
+        1. Add() -
+            a. Check Max Limit
+                --> Read plyrIndex file
+                --> If is 20 then return to Main
+                --> Else continue;
+            b. Take info
+            c. Append to File
+        2. Edit() -
+            a. Who?
+                --> Search function
+                --> populate ref param
+            b. Edit what exactly?
+            c. Enter new, Write to file 
+        3. View() -
+            a. Who?
+                --> Search Function
+                --> populate ref param
+            c. Print to User
+        4. Delete()
+            a. Who? 
+            b. Confirm Person
+            c. Delete / Rewrite without them
+        5. Quit / End While Loop
+    */
+    
+
+    return 0;
+}
+
+
+/*
+Future adjustments:
+    --> Allows user to confirm player being able to see
+            all their data. This is in the event of 2 players
+            having nearly identical info.
+*/
